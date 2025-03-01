5d52003258b788aec44df2490ba359a7f0537c2e
siffahim <siffahim25@gmail.com>
2025-03-01 15:06:11 +0600

    :update:

--- index.ts
+++ index.ts
@@ -74,6 +74,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 //listening
-app.listen(Number(process.env.PORT), "10.0.70.80", () => {
+app.listen(Number(process.env.PORT), () => {
   console.log(`Application running port: ${process.env.PORT}`);
 });
